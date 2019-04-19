41480555fbf1acfc88a944cb88ff38401a71670f
Druidan <44009123+Druidan@users.noreply.github.com>
2019-04-19 11:54:22 -0500

    Slight updates to home page

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3010;
 // St6andard Express setup for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
