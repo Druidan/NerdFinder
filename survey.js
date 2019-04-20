abea0fd6e16c95f95a1de7f34b27b59c03c848be
Druidan <44009123+Druidan@users.noreply.github.com>
2019-04-20 14:10:21 -0500

    Finished the final comparison function. Now to make the modal display function and the CSS.

--- survey.js
+++ survey.js
@@ -1,25 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function () {
     //My JS starts past this point.
 
-//Website States
-let searching = false;
+    //Website States
+    let searching = false;
 
-//Grab HTML Elements
-const surveyDiv = document.querySelectorAll(`.surveyDiv`);
-const surveySubmitBtn = document.querySelectorAll(`.submitSurvBtn`);
+    //Global Variables
+    let matches = [];
+    let numQs = 0;
+    let largestDiff;
 
-function buildQs() {
-    i = 0;
-    axios.get(`/api/surveyQs`).then(surveyQs => {
-        results = surveyQs.data
-        results.forEach(q => {
-            i++
-            const qText = q.question;
-            const newQ = document.createElement(`div`);
-            const qTextHeader = document.createElement(`h3`);
-            const qContent = document.createElement(`p`);
-            const dropMenu = document.createElement(`div`);
-            const dirtySelector = `
+    //Grab HTML Elements
+    const surveyDiv = document.querySelectorAll(`.surveyDiv`);
+    const surveySubmitBtn = document.querySelectorAll(`.submitSurvBtn`);
+
+    function buildQs() {
+        axios.get(`/api/surveyQs`).then(surveyQs => {
+            results = surveyQs.data
+            results.forEach(q => {
+                numQs++
+                const qText = q.question;
+                const newQ = document.createElement(`div`);
+                const qTextHeader = document.createElement(`h3`);
+                const qContent = document.createElement(`p`);
+                const dropMenu = document.createElement(`div`);
+                const dirtySelector = `
                 <select class='dropMenu'>
                     <option value=""></option>
                     <option value="1">1 (Strongly Disagree)</option>
@@ -28,90 +32,103 @@ function buildQs() {
                     <option value="4">4</option>
                     <option value="5">5 (Strongly Agree)</option>
                 </select>`
-            const cleanSelector = DOMPurify.sanitize(dirtySelector);
-            newQ.className = `qDiv q${i}Div`;
-            qTextHeader.className = `qHead`;
-            qTextHeader.textContent = `Question ${i}`;
-            newQ.appendChild(qTextHeader);
-            qContent.className = `qContent`;
-            qContent.textContent = qText;
-            newQ.appendChild(qContent);
-            dropMenu.innerHTML = cleanSelector;
-            newQ.appendChild(dropMenu);
-            surveyDiv.forEach(elem => {
-                elem.appendChild(newQ);
-            })
-        });
-    }) 
-}
-buildQs();
-
-surveySubmitBtn.forEach(function(elm){ 
-    searching = true;
-    elm.addEventListener('click', function(){
-        event.preventDefault();
-        const formBoxes = document.querySelectorAll(`.formBox`);
-        const dropMenues = document.querySelectorAll(`.dropMenu`);
-        
-        function emptyCheck() {
-            let allFull = true;
-
-            formBoxes.forEach( form => {
-                if(form.value === ""){
-                    allFull = false;
-                };
+                const cleanSelector = DOMPurify.sanitize(dirtySelector);
+                newQ.className = `qDiv q${numQs}Div`;
+                qTextHeader.className = `qHead`;
+                qTextHeader.textContent = `Question ${numQs}`;
+                newQ.appendChild(qTextHeader);
+                qContent.className = `qContent`;
+                qContent.textContent = qText;
+                newQ.appendChild(qContent);
+                dropMenu.innerHTML = cleanSelector;
+                newQ.appendChild(dropMenu);
+                surveyDiv.forEach(elem => {
+                    elem.appendChild(newQ);
+                })
             });
-
-            dropMenues.forEach( menu => {
-                if(menu.value === ""){
-                    allFull = false;
-                };
-            });
-
-            return allFull;
-        };
-
-        if(emptyCheck()) {
-            const thisUserName = document.querySelectorAll(`.name-input`);
-            const thisUserPic = document.querySelectorAll(`.pic-link-input`);
-            const thisUser = {
-                name: thisUserName[0].value,
-                photo: thisUserPic[0].value,
-                scores: [],
-            }
-            dropMenues.forEach( result => {
-                thisUser.scores.push(parseInt(result.value));
-            });
-
-            axios.post(`/api/nerdFriends`, thisUser).then( function (response) {
-                bestMatchCheck(thisUser)
-            }).catch(function (error) {
-                console.log(error);
-            });
-
-
-
-        } else {
-            alert(`Please fill out all of the survey!`)
-            searching = false
-        }
-
-    }, false);
-});
-
-function bestMatchCheck(userObj) {
-    const thisUserName = userObj.name;
-    const thisUserArr = userObj.scores;
-    console.log(thisUserArr);
-
-    axios.get(`/api/nerdfriends`).then( response => {
-        response.forEach( nerd => {
-            if(nerd.name !== thisUserName){
-                
-            }
+            largestDiff = numQs * 4
         })
-    })
-}
-        
-//My JS Ends beyond this point.
+    }
+    buildQs();
+
+    surveySubmitBtn.forEach(function (elm) {
+        searching = true;
+        elm.addEventListener('click', function () {
+            event.preventDefault();
+            const formBoxes = document.querySelectorAll(`.formBox`);
+            const dropMenues = document.querySelectorAll(`.dropMenu`);
+
+            function emptyCheck() {
+                let allFull = true;
+
+                formBoxes.forEach(form => {
+                    if (form.value === "") {
+                        allFull = false;
+                    };
+                });
+
+                dropMenues.forEach(menu => {
+                    if (menu.value === "") {
+                        allFull = false;
+                    };
+                });
+
+                return allFull;
+            };
+
+            if (emptyCheck()) {
+                const thisUserName = document.querySelectorAll(`.name-input`);
+                const thisUserPic = document.querySelectorAll(`.pic-link-input`);
+                const thisUser = {
+                    name: thisUserName[0].value,
+                    photo: thisUserPic[0].value,
+                    scores: [],
+                }
+                dropMenues.forEach(result => {
+                    thisUser.scores.push(parseInt(result.value));
+                });
+                axios.post(`/api/nerdFriends`, thisUser).then(function (response) {
+                    bestMatchCheck(thisUser)
+                }).catch(function (error) {
+                    console.log(error);
+                });
+            } else {
+                alert(`Please fill out all of the survey!`)
+                searching = false
+            }
+        }, false);
+    });
+
+    function bestMatchCheck(userObj) {
+        const thisUserName = userObj.name;
+        const thisUserArr = userObj.scores;
+
+        axios.get(`/api/nerdFriends`).then(response => {
+            const nerds = response.data
+            console.log(nerds)
+            nerds.forEach(nerd => {
+                console.log(nerd.name)
+                console.log(thisUserName)
+                let diffTotal = 0;
+                i = 0;
+                if (nerd.name !== thisUserName) {
+                    nerd.scores.forEach(score => {
+                        diffTotal = diffTotal + (score + thisUserArr[i]);
+                        i++
+                    })
+                    console.log(diffTotal)
+                    if (diffTotal < largestDiff) {
+                        largestDiff = diffTotal
+                        matches = [];
+                        matches.push(nerd)
+                    } else if (diffTotal === largestDiff) {
+                        matches.push(nerd)
+                    }
+                }
+            })
+            console.log(matches)
+        })
+    }
+
+    //My JS Ends beyond this point.
 });
