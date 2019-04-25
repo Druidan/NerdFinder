32d1c72e9d8a40602a97113c48f34a1374472b95
Druidan <44009123+Druidan@users.noreply.github.com>
2019-04-24 22:17:52 -0500

    Some minor spelling tweaks and a newly filled-out README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # NerdFinder
 A friend finder that focuses on helping you find a nerd friend that shares your interests.
+
+## Overview and Goals
+Nerd Friend Finder is meant to be a lighthearted application to connect one nerd to another who generally shares similar perspectives on nerdy topics.
+
+## MVP
+The MVP of this project is that, when a user visits our site, they will:
+* Be introduced to the app and its core concept and use.
+* Be prompted to take a survey to match them with a nerd friend.
+* When the take survey button is clicked it will take them to the survey page.
+* The survey will populate from our database file.
+* The user will be required to give a name and a link to a picture of themselves.
+* The user will rate their agreement with each statement on the survey from 1 (strongly disagree) to 5 (strongly agree).
+* The user must fill out each blank and rate each statement to complete the survey.
+* The user's overall score will be compared to all other active users' scores, and the active user (or users) with the least difference from our example user's score will be displayed. Ta-da! They have found a friend.
+* They will be given the opportunity to retake the survey.
+
+## Future Features and Fixes
+* Currently the app has no validation on the image url. The user can technically input gibberish. We need to prevent that.
+* The exact methodology for comparison is a bit weak. I think it should add weight to each question based on how important the topic is to the user.
+* We could also use a more thorough survey.
+* The modal could use a carousel to swap between the matching users who share the same score. This is especially likely with our limited question set.
+
+## Dependencies
+* [Express](https://www.npmjs.com/package/express) - We use Express to handle our routing for our pages.
+
+## Deployment 
+We are deploying on Heroku [HERE](https://gentle-fortress-32924.herokuapp.com/)
--- app/data/surveyQs.js
+++ app/data/surveyQs.js
@@ -1,49 +1,52 @@
 const surveyQs = [
     {
-        question: `Cosplay is really freaking cool!`,
+    question: `Cosplay is really freaking cool!`,
     },
     {
-        question: `You're honestly not sure why you're taking this survey right now instead of playing a videogame.`,
+    question: `You're honestly not sure why you're taking this survey right now instead of playing a video game.`,
     },
     {
-        question: `DC is way better than Marvel.`,
+    question: `DC is way better than Marvel.`,
     },
     {
-        question: `You either get hyped or terrified when you hear the words "Roll for initiative."`,
+    question: `You either get hyped or terrified when you hear the words "Roll for initiative."`,
     },
     {
-        question: `Subs are life. Dubs are trash.`,
+    question: `Subs are life. Dubs are trash.`,
     },
     {
-        question: `You know which Star Trek Captain is the best and you are SO ready to throw down over it.`,
+    question: `You know which Star Trek Captain is the best and you are SO ready to throw down over it.`,
     },
     {
-        question: `You have strong opions over over who would win in a fight between Superman and Goku.`,
+    question: `You have strong opinions over over who would win in a fight between Superman and Goku.`,
     },
     {
-        question: `Between old school and new school Dr. Who, you would choose old school every time.`,
+    question: `Between old school and new school Dr. Who, you would choose old school every time.`,
     },
     {
-        question: `You long for the days when it took 10 floppy discs to install a videogame.`,
+    question: `You long for the days when it took 10 floppy discs to install a video game.`,
     },
     {
-        question: `You know exactly how good or bad each Star Wars movie is, and people who disagree with your assessment are getting their tires slashed, I mean... you didn't hear that, okay?`,
+    question: `You know exactly how good or bad each Star Wars movie is, and people who disagree with your assessment are getting their tires slashed, I mean... you didn't hear that, okay?`,
     },
     {
-        question: `You fought in the console wars.`,
+    question: `You fought in the console wars.`,
     },
     {
-        question: `You can explain with passion, at length, why a particular coding library and/or language(s) suck.`,
+    question: `You can explain with passion, at length, why a particular coding library and/or language(s) suck.`,
     },
     {
-        question: `Lord of the Rings is the best fantasy book series ever commited to the page. Period. End of statement.`,
+    question: `Lord of the Rings is the best fantasy book series ever committed to the page. Period. End of statement.`,
     },
     {
-        question: `You've been running the numbers of this survey through some equations and you're pretty sure my methodology is flawed.`,
+    question: `You've been running the numbers of this survey through some equations and you're pretty sure my methodology is flawed.`,
     },
     {
-        question: `You are upset that [insert your favorite nerd thing] isn't mentioned anywhere in this survey, and that its full of mainstream stereotypical fluff.`,
+    question: `You are upset that [insert your favorite nerd thing] isn't mentioned anywhere in this survey, and that it's full of mainstream stereotypical fluff.`,
     },
-]
-
-module.exports = surveyQs;
+    ]
+    
+    module.exports = surveyQs;
+    
+    
+    
